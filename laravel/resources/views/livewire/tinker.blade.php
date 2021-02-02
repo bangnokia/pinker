932547810c12ebe3426c370692f83b77b896cad5
bangnokia <bangnokia@gmail.com>
2021-02-03 02:10:46 +0700

    work in progress

--- laravel/resources/views/livewire/tinker.blade.php
+++ laravel/resources/views/livewire/tinker.blade.php
@@ -21,7 +21,7 @@
 
     @if ($showAddProject)
         <div class="absolute top-1/2 -mt-48 left-0 w-full h-96 flex justify-center z-10">
-            <div class="w-1/2 bg-white p-5">
+            <div class="w-2/3 bg-white p-5">
                 <livewire:select-folder />
             </div>
         </div>
--- serve.js
+++ serve.js
@@ -1,3 +1,4 @@
 const { exec } = require('child_process');
 
-exec('cd laravel && php artisan serve --port=6969');
+exec('cd laravel && php artisan serve --port=6969', function (error, stdout, stderr) {
+});
