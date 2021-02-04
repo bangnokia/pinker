65ddd66d47b755cf54a7ce49d81e827f0524ecb4
bangnokia <bangnokia@gmail.com>
2021-02-04 23:42:07 +0700

    update php server

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const { app, BrowserWindow, nativeImage } = require("electron");
-const { fork } = require("child_process");
+const { fork, exec } = require("child_process");
+const PHPServer = require("php-server-manager");
+const path = require("path");
+
+const server = new PHPServer({
+  directory: path.resolve(__dirname) + "/laravel/public",
+  port: 6969,
+});
 
 function createWindow() {
   const icon = nativeImage.createFromPath(__dirname + "/assets/icon.icns");
@@ -7,23 +14,24 @@ function createWindow() {
     width: 1200,
     height: 690,
     webPreferences: {
-      contextIsolation: true
+      contextIsolation: true,
     },
     icon: icon,
   });
 
-  win.loadFile("index.html");
+  win.loadURL("http://127.0.0.1:6969");
+  win.focus();
 }
 
 app.whenReady().then(() => {
-  const forked = fork('serve.js');
+  server.run();
   createWindow();
 });
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
+    server.close();
     app.quit();
-    forked.kill();
   }
 });
 
